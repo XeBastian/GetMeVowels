62d3156dea8d6b6db32b7619c827d31d074916eb
Xebastian <ernesty533k@gmail.com>
2023-05-26 10:18:25 +0200

    Added user interraction when inputting the char ranges

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,13 +1,20 @@
 ////a program to check the vowels at a specific range of characters in the alphabet
 
-fun main(){
+fun main() {
 //    here we assign a variale to our function call
-    val vowels = getCharacters('a', 'w')
+//    improvement is to get the charachter range in the console.
+    print("Enter the starting Character:   ")
+
+    val start = readLine()!!.toString()[0]
+    print("Enter the end Character:   ")
+    val end = readLine()!!.toString()[0]
+
+    val vowels = getCharacters(start, end)
     //print to get the values from the function
     println(vowels)
 }
 
-fun getCharacters(start: Char, end: Char): MutableList<Char>{
+fun getCharacters(start: Char, end: Char): MutableList<Char> {
 //    1. first we need the vowels array:
     var vowelsArray = mutableListOf<Char>('a', 'e', 'i', 'o', 'u')
 //    2 we need the array which has the start and end charachers that the user provides
@@ -17,7 +24,7 @@ fun getCharacters(start: Char, end: Char): MutableList<Char>{
 
 //    4 the operation to loop through the vowelsArray and check if any of the vowels are in the userArray then add to the empty array
     vowelsArray.forEach {
-        if(it in userRangeArray){
+        if (it in userRangeArray) {
             returnedVowels.add(it)
         }
     }
